66d7b0a37961545f6dfec3a02a403e227dfab6a3
Surbhi <surbhigoel219@gmail.com>
2019-06-08 07:49:45 +0530

    part3

--- events/models.py
+++ events/models.py
@@ -1,3 +1,13 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Candidate(models.Model):
+    name = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    email = models.URLField(default='')
+    bitsid = models.TextField()
+    contact = models.IntegerField(default=0)
+    
+
+    
